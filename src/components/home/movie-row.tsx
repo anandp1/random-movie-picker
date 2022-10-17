479505849dd986bc967e441b7acf318a83249445
Anand Patel <anandpp2001@gmail.com>
2022-10-16 22:17:02 -0600

    fix movie row

--- src/components/home/movie-row.tsx
+++ src/components/home/movie-row.tsx
@@ -29,10 +29,10 @@ const MovieRow: React.FC<MovieRowProps> = ({
         {data.moviesByUser[username].displayName}
       </h2>
       <div className="relative flex items-center group">
-        <div className="group relative hidden sm:block">
+        <div className="group relative hidden sm:block h-full">
           <ChevronLeftIcon
             onClick={slideLeft}
-            className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 text-white"
+            className="absolute top-0 bottom-0 left-2 z-40 m-auto w-10 cursor-pointer opacity-0 text-white group-hover:bg-black group-hover:opacity-100 group-hover:bg-opacity-40 h-full"
           />
         </div>
         <div
@@ -43,10 +43,10 @@ const MovieRow: React.FC<MovieRowProps> = ({
             return <MovieComponent movie={movie} key={movie.title} />;
           })}
         </div>
-        <div className="group relative hidden sm:block">
+        <div className="group relative hidden sm:block h-full">
           <ChevronRightIcon
             onClick={slideRight}
-            className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 text-white"
+            className="absolute top-0 bottom-0 right-[0.4px] z-40 m-auto w-12 cursor-pointer opacity-0 text-white group-hover:bg-black group-hover:opacity-100 group-hover:bg-opacity-40 h-full"
           />
         </div>
       </div>
