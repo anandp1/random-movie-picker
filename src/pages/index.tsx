8db40c82695d1981217b87f7be5250e0da1b4833
Anand Patel <anand@Anands-MBP.hitronhub.home>
2022-12-25 16:04:28 -0700

    Add filter by genre to shuffle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,14 @@ import { fetcher } from "../lib/fetcher";
 
 import MovieRow from "../components/home/movie-row";
 import { getAvailableUsers, MovieByUser, SafeUser } from "../modal/user.modal";
+import { createContext } from "react";
 
 interface HomeProps {
   username: string;
   availableUsers: SafeUser[];
 }
+
+export const DataContext = createContext({})
 
 const Home: NextPage = ({ username, availableUsers }: HomeProps) => {
   const { data, error, mutate } = useSWR<MovieByUser>(
@@ -54,26 +57,29 @@ const Home: NextPage = ({ username, availableUsers }: HomeProps) => {
   ];
 
   return (
-    <Layout
-      username={username}
-      mutateUserData={mutate}
-      availableUsers={availableUsers}
-    >
-      <div className="flex flex-col">
-        {loggedInUserFirstArray.map((username: string, index: number) => {
-          return (
-            <MovieRow
-              key={username}
-              username={username}
-              yourUsername={yourUsername}
-              randomId={index}
-              data={data}
-              mutateUserData={mutate}
-            />
-          );
-        })}
-      </div>
-    </Layout>
+    <DataContext.Provider value={data.moviesByUser}>
+      <Layout
+        username={username}
+        mutateUserData={mutate}
+        availableUsers={availableUsers}
+      >
+        <div className="flex flex-col">
+          {loggedInUserFirstArray.map((username: string, index: number) => {
+            return (
+              <MovieRow
+                key={username}
+                username={username}
+                yourUsername={yourUsername}
+                randomId={index}
+                data={data}
+                mutateUserData={mutate}
+              />
+            );
+          })}
+        </div>
+      </Layout>
+    </DataContext.Provider>
+
   );
 };
 
